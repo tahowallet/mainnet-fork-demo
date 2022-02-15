9b2f433443ccbd59ea846a44c5d78d4d35e0319f
Gergo Nagy <gergo.nagy@tally.cash>
2022-02-15 14:30:08 -0700

    init setup

---
+++ test/sample-test.js
@@ -0,0 +1,24 @@
+const { expect } = require("chai");
+const { ethers, ethernal } = require("hardhat");
+
+describe("Greeter", function () {
+  it("Should return the new greeting once it's changed", async function () {
+    const Greeter = await ethers.getContractFactory("Greeter");
+    const greeter = await Greeter.deploy("Hello, world!");
+    await greeter.deployed();
+
+    await ethernal.push({
+      name: 'Greeter',
+      address: greeter.address
+  });
+
+    expect(await greeter.greet()).to.equal("Hello, world!");
+
+    const setGreetingTx = await greeter.setGreeting("Hola, mundo!");
+
+    // wait until the transaction is mined
+    await setGreetingTx.wait();
+
+    expect(await greeter.greet()).to.equal("Hola, mundo!");
+  });
+});
